f4cd07e7a6d2b2e26bd352d210eee5b9588a2e57
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-10 11:31:04 -0400

    Beginning lab, set language, sdk and compiler to allow project to compile properly. Added initial movie table specified in README

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -35,6 +35,13 @@ CREATE TABLE movies (
 );
 
 -- Tables for in-class example
+INSERT INTO movies (title, runtime, genre, imdb_score, rating)
+VALUES ('Howard the Duck', 110, 'Sci-Fi', 4.6, 'PG'),
+       ('Lavalantula', 83, 'Horror', 4.7, 'TV-14'),
+       ('Starship Troopers', 129, 'Sci-Fi', 7.2, 'PG-13'),
+       ('Waltz With Bashir', 90, 'Documentary', 8.0, 'R'),
+       ('Spaceballs', 96, 'Comedy', 7.1, 'PG'),
+       ('Monsters Inc.', 92, 'Animation', 8.1, 'G');
 
 DROP TABLE IF EXISTS cars;
 
