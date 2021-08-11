17b2e1ab11f2c0cbdac0b2229ea7673f33c7d7e1
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-08-11 08:21:12 -0400

    Finished section 1, all queries in script.sql file

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -4,3 +4,5 @@ Select * from movies where RATING IN ('G', 'PG') AND RUNTIME < 100;
 Select AVG(RUNTIME), TITLE, GENRE FROM movies WHERE IMDB_SCORE < 7.5 GROUP BY GENRE, TITLE;
 UPDATE movies SET RATING = 'R' WHERE TITLE = 'Starship Troopers';
 Select ID, RATING FROM movies WHERE GENRE IN ('Horror', 'Documentary');
+SELECT genre, AVG(IMDB_SCORE) AS Average, MIN(IMDB_SCORE) AS Min, MAX(IMDB_SCORE) AS Max FROM movies  GROUP BY genre HAVING COUNT(*) > 1;
+DELETE FROM movies WHERE RATING = 'R';
